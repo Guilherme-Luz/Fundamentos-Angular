628625b62f600d44ae0bc1d9020dee79a0d97930
Guilherme Luz <guilherme.luz2806@gmail.com>
2024-06-18 11:41:14 +0100

    Fundamentos Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,74 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fundamentos-Angular';
+  inputT = document.getElementById("meu-input");
+
+  inputType = "text";
+  inputText = "Texto Inicial";
+  isDisabled = true;
+
+  buttonTitle = "Titulo do Botao";
+  buttonDisable = true;
+
+
+
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+  disableInput(){
+    this.isDisabled = true;
+  }
+
+  inputSenha() {
+    this.inputType = "password";
+  }
+  inputTexto(){
+    this.inputType = "text";
+  }
+
+  LogInputText(){    
+    console.log(this.inputT);
+  }
+
+  handleInputKey(event: Event){
+    const currentText = (event.target as HTMLInputElement).value;
+    console.log(currentText);
+  }
+
+  enableButton(){
+    if(this.buttonDisable == true){
+      this.buttonDisable = false;
+    }else{
+      this.buttonDisable = true;
+    }
+  }
+
+  //Style Binding
+
+    widthButton1 = '110px';
+    widthButton2 = 120;
+  
+    stylesObj = {
+      width: '160px',
+      backgroundColor: 'grey',
+    }
+
+  updateStyleObj(){
+    this.stylesObj.width = '170px';
+    this.stylesObj.backgroundColor = 'lightblue';
+  }
+  updateStyleObjCorrect(){
+    this.stylesObj = {
+      width: '170px',
+      backgroundColor: 'lightblue'
+    }
+  }
+  updateWidth(){
+    this.widthButton2 = 200;
+  }
+
+  //Class Binding
+  isRedButton = true;
+  isGreen = true;
 }
